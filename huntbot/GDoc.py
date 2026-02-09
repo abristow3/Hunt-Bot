806f6971f227581da016dfb2a1baf4899f14e142
Alex <alex.bristow3@gmail.com>
2026-02-09 17:41:52 -0500

    Added write_to_shet() method to GDoc class

--- huntbot/GDoc.py
+++ huntbot/GDoc.py
@@ -1,6 +1,5 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-import yaml
 import os
 import logging
 
@@ -19,29 +18,27 @@ class GDoc:
     def on_startup(self) -> None:
         try:
             # Load the service account credentials from the JSON file
-            self.creds_path = os.getenv("GOOGLE_CREDENTIALS_PATH")
-
-            logger.info("GOOGLE CREDS PATH: " + self.creds_path)
+            self.creds_path = os.getenv("GOOGLE_CREDENTIALS_PATH", "")
+            logger.info(f"[GDoc] GOOGLE CREDS PATH: {self.creds_path}")
 
             if not self.creds_path:
-                logger.error("Missing GOOGLE_CREDENTIALS_PATH value")
+                logger.error("[GDoc] Missing GOOGLE_CREDENTIALS_PATH value")
 
-            logger.info("Google Credentials found, attempting to establish connection to GDocs API server")
+            logger.info("[GDoc] Google Credentials found, attempting to establish connection to GDocs API server")
             self.credentials = service_account.Credentials.from_service_account_file(self.creds_path, scopes=[
-                "https://www.googleapis.com/auth/spreadsheets.readonly"], )
+                "https://www.googleapis.com/auth/spreadsheets"], )
 
             # Build the Sheets API client
             self.service = build("sheets", "v4", credentials=self.credentials)
             self.sheets = self.service.spreadsheets()
         except Exception as e:
-            logger.error(e)
-            logger.error("Error during GDoc object setup")
+            logger.error("[GDoc] Error during GDoc object setup", exc_info=e)
 
     def set_sheet_id(self, sheet_id: str) -> None:
         self.sheet_id = sheet_id
 
     def get_data_from_sheet(self, sheet_name: str, cell_range: str = None) -> list:
-        logger.info("Retrieving data from GDoc sheet...")
+        logger.info("[GDoc] Retrieving data from GDoc sheet...")
         try:
             if not cell_range:
                 data = self.sheets.values().get(spreadsheetId=self.sheet_id, range=sheet_name).execute()
@@ -53,11 +50,23 @@ class GDoc:
 
             return data
         except Exception as e:
-            logger.error(e)
-            logger.error("Unable to get data from GDoc sheet")
+            logger.error("[GDoc] Unable to get data from GDoc sheet", exc_info=e)
             return []
 
     @staticmethod
     def a1notation_builder(sheet_name: str, cell_range: str) -> str:
         a1format = f"{sheet_name}!{cell_range}"
         return a1format
+
+    def write_to_sheet(self, spreadsheet_id: str, sheet_name: str, cell: str, value) -> bool:
+        try:
+            a1_range = self.a1notation_builder(sheet_name, cell)
+            body = {"values": [[value]]}
+
+            self.sheets.values().update(spreadsheetId=spreadsheet_id, range=a1_range, valueInputOption="RAW",
+                                        body=body).execute()
+
+            return True
+        except Exception as e:
+            logger.error("[Gdoc] Unable to write to Flux RL Plugin Config GDoc sheet", exc_info=e)
+            return False
